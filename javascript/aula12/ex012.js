b25c5f46bdc4c2b832972aca100c1b2f17f4f12d
Giulia Castro <giuliareis21@gmail.com>
2026-03-29 20:45:21 -0300

    .

---
+++ javascript/aula12/ex012.js
@@ -0,0 +1,12 @@
+let agora = new Date()
+let hora = agora.getHours()
+let minutos = agora.getMinutes()
+
+console.log(`Agora são exatamente ${hora} horas e ${minutos} minutos.`)
+if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora <= 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
---
+++ javascript/exercicio/exe02/script.js
@@ -0,0 +1,7 @@
+function verificar() {
+    let data = new Date()
+    let ano = data.getFullYear()
+    const fano = document.getElementById ('txtano')
+    const res = document.querySelector ('div#resultado')
+}
+if (fano.value.length ==0 || )
